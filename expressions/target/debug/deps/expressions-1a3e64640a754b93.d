1d322069ede360ba6c74b61fa11163c8370f882b
imaad666 <imaadbinirshad@gmail.com>
2025-10-15 23:15:59 +0530

    practice

---
+++ expressions/target/debug/deps/expressions-1a3e64640a754b93.d
@@ -0,0 +1,5 @@
+/Users/imaad/i/rust book/expressions/target/debug/deps/expressions-1a3e64640a754b93.d: src/main.rs
+
+/Users/imaad/i/rust book/expressions/target/debug/deps/libexpressions-1a3e64640a754b93.rmeta: src/main.rs
+
+src/main.rs:
---
+++ expressions/target/debug/deps/expressions-8ea32c208532ed0e.d
@@ -0,0 +1,5 @@
+/Users/imaad/i/rust book/expressions/target/debug/deps/expressions-8ea32c208532ed0e.d: src/main.rs
+
+/Users/imaad/i/rust book/expressions/target/debug/deps/libexpressions-8ea32c208532ed0e.rmeta: src/main.rs
+
+src/main.rs:
