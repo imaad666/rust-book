5cc78c335c394f95d409aa5d91cd912967a6fcfe
imaad666 <imaadbinirshad@gmail.com>
2025-10-16 16:10:11 +0530

    practice

---
+++ example/target/debug/deps/example-1a94191479291df7.d
@@ -0,0 +1,5 @@
+/Users/imaad/i/rust book/example/target/debug/deps/example-1a94191479291df7.d: src/main.rs
+
+/Users/imaad/i/rust book/example/target/debug/deps/libexample-1a94191479291df7.rmeta: src/main.rs
+
+src/main.rs:
---
+++ example/target/debug/deps/example-6e803a1bd338330f.d
@@ -0,0 +1,5 @@
+/Users/imaad/i/rust book/example/target/debug/deps/example-6e803a1bd338330f.d: src/main.rs
+
+/Users/imaad/i/rust book/example/target/debug/deps/libexample-6e803a1bd338330f.rmeta: src/main.rs
+
+src/main.rs:
